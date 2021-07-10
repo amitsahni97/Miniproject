7e9cf344bc44f87aaec9592f79543ebc15b7cbb6
mansitardeja <68369404+mansitardeja@users.noreply.github.com>
2021-07-10 17:45:43 +0530

    Add files via upload

---
+++ 3_Implementation/functions.h
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+int add(int a,int b)
+{
+    printf("Enter the value of a and b : \n");
+    scanf("%d %d",&a,&b);
+    int result=a+b;
+    return result;
+}
+int sub(int a,int b)
+{
+    printf("Enter the value of a and b : \n");
+    scanf("%d %d",&a,&b);
+    int result=a-b;
+    return result;
+}
+int mul(int a,int b)
+{
+    printf("Enter the value of a and b : \n");
+    scanf("%d %d",&a,&b);
+    int result=a*b;
+    return result;
+}
+int division(int a,int b)
+{
+    printf("Enter the value of a and b : \n");
+    scanf("%d %d",&a,&b);
+    int result=a/b;
+    return result;
+}
+int remain(int a,int b)
+{
+    printf("Enter the value of a and b : \n");
+    scanf("%d %d",&a,&b);
+    int result=a%b;
+    return result;
+}
+int factorial(int a)
+{
+    printf("Enter the number whose factorial you wanna find  : \n");
+    scanf("%d %d",&a);
+    int factorial=1,result;
+    for(int i=1;i<=a;i++)
+    {
+        factorial=factorial*i;
+    }
+    return factorial;
+}
+ 
+
+#endif
---
+++ 3_Implementation/tempCodeRunnerFile.c
@@ -0,0 +1,1 @@
+int FACTORIAL(int v1)
